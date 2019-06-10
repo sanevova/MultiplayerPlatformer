2d79377abb859af1e93acf34bb17b6e4dd7773b4
Vova Mishatkin <sane.vova@gmail.com>
2019-06-10 01:49:21 +0100

    add cors, https, allways conn to aws

--- public/client/client.js
+++ public/client/client.js
@@ -2,14 +2,14 @@ function main() {
 
 
   if (window.location.hostname === 'localhost') {
-      rhost = 'localhost';
-      rport = 3001;
-  } else {
-      rhost = 'aws_ec2.pem ubuntu@ec2-35-177-47-61.eu-west-2.compute.amazonaws.com';
+  //     rhost = 'localhost';
+  //     rport = 3001;
+  // } else {
+      rhost = 'ec2-35-177-47-61.eu-west-2.compute.amazonaws.com';
       rport = 8303;
   }
 
-  var socket = io(`http://${rhost}:${rport}`);
+  var socket = io(`https://${rhost}:${rport}`);
 
   socket.on('new YUNG', (data) => {
     console.log('server recevd yung connected', data.name);
--- server/chat/server.js
+++ server/chat/server.js
@@ -17,6 +17,14 @@ server.listen(port, () => {
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   res.header("Access-Control-Allow-Headers", "Content-Type");
+   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+   next();
+});
+
 var numUsers = 0;
 console.log("ROFL");
 io.on('connection', (socket) => {
